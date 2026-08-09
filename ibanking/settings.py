7c86277eee55a8e960ed513950745a18eb4bc4b0
Kavita Singh Panwar <kavitasinghpanwar33@gmail.com>
2026-08-09 23:18:55 +0530

    Configure CSRF trusted origins for deployment

--- ibanking/settings.py
+++ ibanking/settings.py
@@ -152,10 +152,10 @@ LOGOUT_REDIRECT_URL = '/login/'
 
 # ---------------- CSRF FIX ---------------- #
 
-CSRF_TRUSTED_ORIGINS = [
-    "http://127.0.0.1:8000",
-    "http://localhost:8000",
-]
+CSRF_TRUSTED_ORIGINS = os.environ.get(
+    'CSRF_TRUSTED_ORIGINS',
+    'http://127.0.0.1:8000,http://localhost:8000'
+).split(',')
 
 
 # ---------------- RECAPTCHA ---------------- #
